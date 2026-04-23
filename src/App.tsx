b2b84b6407fbd626a2ae127586fc15459f6abc71
rosendo <rosendc30@gmail.com>
2026-04-22 22:55:02 -0300

    LAYOUT DESKTOP

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import { Card } from "./components/Card"
 import { Header } from "./components/Header"
 import { Title } from "./components/Title"
-import Iconlist from "./assets/icon-list.svg"
+import IconList from "./assets/icon-list.svg"
 import { Input } from "./components/Input"
 import { Button } from "./components/Button"
 
+
 export function App() {
   return (
-    <div className="bg-white max-w-[375px] mx-auto flex flex-col items-center border">
-      <main >
+    <div className=" sm:mx-auto sm:flex sm:flex-col sm:items-center lg:max-w-200 lg:rounded-4xl">
+      <main className="sm:bg-white lg:flex flex-row-reverse">
         <Header />
-        <div className="px-6">
+        <div className="sm:px-6 lg:py-12 lg:w-190 ">
           <Title>Stay updates!</Title>
           <Card>Join 60,000+ product managers receiving monthly updates on:</Card>
-          <Card svgSrc={Iconlist}> Product discovery and building what matters</Card>
-          <Card svgSrc={Iconlist}>Measuring to ensure updates are a success</Card>
-          <Card svgSrc={Iconlist}>And much more!</Card>
+          <Card svgSrc={IconList}>Product discovery and building what matters</Card>
+          <Card svgSrc={IconList}>Measuring to ensure updates are a success</Card>
+          <Card svgSrc={IconList}>And much more!</Card>
           <form action="" className="mt-10">
             <Input />
-            <Button>Subscribe to monthly newsletter</Button>
+            <Button >Subscribe to monthly newsletter</Button>
           </form>
         </div>
       </main>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
-import BgMobile from "../assets/illustration-sign-up-mobile.svg"
-
-export function Header() {
+import MobileImg from "../assets/illustration-sign-up-mobile.svg"
+import DesktopImg from "../assets/illustration-sign-up-desktop.svg"
+type HeaderProps = {
+    children?: string
+}
+export function Header({ children }: HeaderProps) {
     return (
-        <div>
-            <img className="w-full " src={BgMobile} alt="" />
-        </div>
+        <header className="lg:w-full lg:flex">
+            {children}
+            <img className="w-full lg:hidden" src={MobileImg} alt="mobile image" />
+            <img className="sm:hidden lg:block lg:w-95" src={DesktopImg} alt="desktop image" />
+        </header>
     )
 }
